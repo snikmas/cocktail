254510286bed026a4d2b134de0375d34c0b2916a
MarySnik <mary.snikk@gmail.com>
2025-01-31 18:49:50 +0800

    search 1.0

--- script.js
+++ script.js
@@ -2,15 +2,27 @@
 const leftBtn = document.querySelector('#left');
 const rightBtn = document.querySelector('#right');
 const randomBtn = document.querySelector("#random-btn");
-const inputBtn = document.querySelector("#cocktail-btn")
+const inputBtn = document.querySelector("#cocktail-btn");
+const inputForm = document.querySelector('#cocktail-in');
+
+const resultsDiv = document.querySelector(".results");
 
 // module.exports = {inputBtn};  can we?
 
 leftBtn.addEventListener('click', leftCarousel);
 rightBtn.addEventListener('click', rightCarousel);
 randomBtn.addEventListener('click', generateDrinks);
-inputBtn.addEventListener('click', inputDrink);
 
+inputBtn.addEventListener('click', inputDrink);
+inputForm.addEventListener('keydown', keyPress => {
+  console.log(keyPress)
+  
+  if (keyPress.key == "Enter") {
+    keyPress.preventDefault()
+    inputDrink()
+  }
+  
+})
 
 // 1. input drink and take this input
 // 2. find drinks which match this input, create array of these drinks
@@ -24,53 +36,77 @@ inputBtn.addEventListener('click', inputDrink);
 function inputDrink(){
   
   let nameDrink = document.querySelector('#cocktail-in').value;
-  window.location.href ='results.html';
+  
+  let drinks = {};
+  let k = 0;
+  console.log(nameDrink)
+  
+  //need to add elements results input gallery elements
+  let resultsElements = document.createElement("div");
+  resultsElements.classList.add("results");
+  resultsElements.innerHTML = `
+    <div class="input">
+    <h3>Your results: Margarita</h3>
+  </div>
+  <div class="gallery">
+  </div>
+  `
+  document.body.appendChild(resultsElements);
+  
+  const gallery = document.querySelector(".gallery");
   
   
-  // let drinks = {};
-  // let k = 0;
-  // console.log(nameDrink)
-  // fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${nameDrink}`)
-  // .then(res => res.json())
-  // .then(data => {
-    //   console.log(data);
-    //   while(data[k]){
-      //     drinks.push(data[k]);
-      //     k++;
-      //   }
+  fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${nameDrink}`)
+  .then(res => res.json())
+  .then(data => {
+    console.log(data);
+  while(data[k]){
+    drinks.push(data[k]);
+    k++;
+  }
       
-      //   console.log(drinks)
+  console.log(drinks)
+
+  gallery.innerHTML = '';
+  for (let i = 0; i < drinks.length; i++){
+    gallery.innerHTML += `
+      <div class="gallery-div">
+        <img class="gallery-img" src="${drinks[i].strDrinkThumb}">
+        <h6 class="gallery-name">${drinks[i].strDrink}</h6>
+      </div>
+    `
+  }  
+  }
+  )
+  .catch(error => {
+    console.log(`Error: ${error}`)
+  })
+        
+}
       
-      // }
-      // )
-      // .catch(error => {
-        //   console.log(`Error: ${error}`)
-        // })
-        
-      }
-      
-      
-      // 1. the center drink moves to the right
-      // 2. the left drink moves to the center
-      // 3. get new left drink
-      // DONE
-      function leftCarousel(){
-        
-        document.querySelector('.drinkImg3').src = document.querySelector(".drinkImg2").src;
-        document.querySelector('.drinkName3').innerText = document.querySelector(".drinkName2").innerText;
-        
-        document.querySelector('.drinkImg2').src = document.querySelector(".drinkImg1").src;
-        document.querySelector('.drinkName2').innerText = document.querySelector(".drinkName1").innerText;
-        
-        generateDrink(1)
-      }
+    
+
+// 1. the center drink moves to the right
+// 2. the left drink moves to the center
+// 3. get new left drink
+// DONE
+function leftCarousel(){
+  
+  document.querySelector('.drinkImg3').src = document.querySelector(".drinkImg2").src;
+  document.querySelector('.drinkName3').innerText = document.querySelector(".drinkName2").innerText;
+  
+  document.querySelector('.drinkImg2').src = document.querySelector(".drinkImg1").src;
+  document.querySelector('.drinkName2').innerText = document.querySelector(".drinkName1").innerText;
+  
+  generateDrink(1)
+}
       
       // class bug -> class returns array, need one value // SOLVED
-      function rightCarousel(){
-        
-        document.querySelector('.drinkImg1').src = document.querySelector(".drinkImg2").src;
-        document.querySelector('.drinkName1').innerText = document.querySelector(".drinkName2").innerText;
-        
+function rightCarousel(){
+
+  document.querySelector('.drinkImg1').src = document.querySelector(".drinkImg2").src;
+  document.querySelector('.drinkName1').innerText = document.querySelector(".drinkName2").innerText;
+
   document.querySelector('.drinkImg2').src = document.querySelector(".drinkImg3").src;
   document.querySelector('.drinkName2').innerText = document.querySelector(".drinkName3").innerText;
   
@@ -109,6 +145,6 @@ function generateDrinks(){
   }
 }
 
-// generateDrinks()
+generateDrinks()
 
-export {nameDrink}
+// export {nameDrink}?????
---
+++ test.js
@@ -0,0 +1,4 @@
+const newDiv = document.createElement('div')
+newDiv.classList.add("hiis")
+newDiv.innerText = "I'm alive"
+document.body.appendChild(newDiv)
